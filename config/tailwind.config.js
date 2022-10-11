cdfcf6ea228065b7a642f327034784dc6aec173a
DavidAshburn <oneinchwebbing@gmail.com>
2022-10-11 16:13:59 -0700

    nav and alert partials, tailwind config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,8 +9,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundSize: {
+        'oversize': '400%',
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        'raleway': ['Raleway', 'sans-serif'],
+        'monoton': ['Monoton', 'cursive'],
+        'righteous': ['Righteous', 'cursive'],
       },
     },
   },
