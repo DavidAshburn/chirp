906965b088add8c60ddad4f9ca4c6511a469a1e4
DavidAshburn <oneinchwebbing@gmail.com>
2022-10-11 16:08:16 -0700

    initial commit

---
+++ db/migrate/20221011230415_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false, default: ""
+      t.string :password_digest, null: false, default: ""
+      t.integer :role
+      t.string :username
+
+      t.timestamps
+    end
+  end
+end
