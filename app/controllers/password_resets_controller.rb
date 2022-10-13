de4aeeea27c9a2b8936c1a9461466d719211404b
DavidAshburn <oneinchwebbing@gmail.com>
2022-10-13 15:40:46 -0700

    password resets working, local mailer doesnt send of course

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,8 +1,33 @@
 class PasswordResetsController < ApplicationController
-	
 	def new
 	end
 
 	def create
+		@user = User.find_by(email: params[:email])
+		if @user.present?
+			PasswordMailer.with(user: @user).reset.deliver_now
+		end
+		redirect_to root_path, notice: "A reset link was sent if the e-mail matches an account."
+	end
+
+	def edit
+		@user = User.find_signed!(params[:token], purpose: "password_reset")
+	rescue ActiveSupport::MessageVerifier::InvalidSignature
+		redirect_to sign_in__path, alert: "Your token has expired, please reset again."
+	end
+
+	def update
+		@user = User.find_signed!(params[:token], purpose: "password_reset")
+		if @user.update(password_params)
+			redirect_to sign_in_path, notice: "Your password was reset."
+		else 
+			render :edit, status: :unprocessable_entity
+		end
+	end
+
+	private
+
+	def password_params
+		params.require(:user).permit(:password, :password_confirmation)
 	end
 end
